aa86eb5709040c81403c1c963767beadb0de92ec
Dzmitry Shalukhau <ivol84@gmail.com>
2017-10-23 21:40:52 +0300

    Initial commit.

---
+++ src/main/java/com/gmail/ivol84/javarush_intership_test_task/JavarushIntershipTestTaskApplication.java
@@ -0,0 +1,12 @@
+package com.gmail.ivol84.javarush_intership_test_task;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavarushIntershipTestTaskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavarushIntershipTestTaskApplication.class, args);
+	}
+}
---
+++ src/test/java/com/gmail/ivol84/javarush_intership_test_task/JavarushIntershipTestTaskApplicationTests.java
@@ -0,0 +1,16 @@
+package com.gmail.ivol84.javarush_intership_test_task;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class JavarushIntershipTestTaskApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
